964eb7ef0aa0b342a4ea66fc382c959c66d36da0
ErikRojaas <152431347+ErikRojaas@users.noreply.github.com>
2024-10-23 18:42:11 +0200

    Layouts y demás

--- Proxmox/src/main/java/com/project/ClientFX.java
+++ Proxmox/src/main/java/com/project/ClientFX.java
@@ -15,6 +15,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -38,8 +39,8 @@ public class ClientFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        final int windowWidth = 655;
-        final int windowHeight = 430;
+        final int windowWidth = 672;
+        final int windowHeight = 431;
 
         UtilsViews.parentContainer.setStyle("-fx-font: 14 arial;");
         UtilsViews.addView(getClass(), "ViewConfig", "/assets/viewConfig.fxml");
@@ -51,6 +52,7 @@ public class ClientFX extends Application {
         ctrlPlay = (CtrlPlay) UtilsViews.getController("ViewPlay");
 
         Scene scene = new Scene(UtilsViews.parentContainer);
+        
 
         scene.widthProperty().addListener(new ChangeListener<Number>() {
             @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
@@ -71,6 +73,8 @@ public class ClientFX extends Application {
         stage.setMinHeight(windowHeight);
         //stage.setMaxHeight(windowHeight);
         stage.show();
+
+
 
         stage.widthProperty().addListener((obs, oldVal, newVal) -> {
             System.out.println("Width changed: " + newVal);
